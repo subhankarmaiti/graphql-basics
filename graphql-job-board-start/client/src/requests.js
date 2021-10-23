4d3956039af5f860b098072edf4cc92b9e7ad82c
Subhankar Maiti <subhankaraiti40@gmail.com>
2021-10-23 23:57:22 +0530

    added authorization to apollo client

--- graphql-job-board-start/client/src/requests.js
+++ graphql-job-board-start/client/src/requests.js
@@ -1,31 +1,28 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import {
+  ApolloClient,
+  HttpLink,
+  ApolloLink,
+  InMemoryCache,
+} from "apollo-boost";
 import gql from "graphql-tag";
 import { isLoggedIn, getAccessToken } from "./auth";
 const endpointURL = "http://localhost:9000/graphql";
 
+const authLink = new ApolloLink((operation, forward) => {
+  if (isLoggedIn) {
+    operation.setContext({
+      headers: {
+        authorization: "Bearer " + getAccessToken(),
+      },
+    });
+  }
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: endpointURL }),
+  link: ApolloLink.from([authLink, new HttpLink({ uri: endpointURL })]),
   cache: new InMemoryCache(),
 });
-export async function graphqlRequest(query, variables) {
-  const request = {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ query, variables }),
-  };
-  if (isLoggedIn) {
-    request.headers["authorization"] = "Bearer " + getAccessToken();
-  }
-  const response = await fetch(endpointURL, request);
-  const responseBody = await response.json();
-  if (responseBody.errors) {
-    const message = responseBody.errors
-      .map((error) => error.message)
-      .join("\n");
-    throw new Error(message);
-  }
-  return responseBody.data;
-}
 
 export async function loadJobs() {
   const query = gql`
