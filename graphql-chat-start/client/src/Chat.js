b891b22ee66cab0ccd8c5ff0cdc74c840d150074
Subhankar Maiti <subhankaraiti40@gmail.com>
2021-10-24 19:21:42 +0530

    subscription added to client

--- graphql-chat-start/client/src/Chat.js
+++ graphql-chat-start/client/src/Chat.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import { addMessage, getMessages } from './graphql/queries';
-import MessageInput from './MessageInput';
-import MessageList from './MessageList';
+import React, { Component } from "react";
+import { addMessage, getMessages, onMessageAdded } from "./graphql/queries";
+import MessageInput from "./MessageInput";
+import MessageList from "./MessageList";
 
 class Chat extends Component {
-  state = {messages: []};
-
+  state = { messages: [] };
+  subscription = null;
   async componentDidMount() {
     const messages = await getMessages();
-    this.setState({messages});
+    this.setState({ messages });
+    this.subscription = onMessageAdded((message) => {
+      this.setState({ messages: this.state.messages.concat(message) });
+    });
   }
-
+  componentWillUnmount() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
   async handleSend(text) {
-    const message = await addMessage(text);
-    this.setState({messages: this.state.messages.concat(message)});
+    await addMessage(text);
   }
 
   render() {
-    const {user} = this.props;
-    const {messages} = this.state;
+    const { user } = this.props;
+    const { messages } = this.state;
     return (
       <section className="section">
         <div className="container">
@@ -28,7 +34,7 @@ class Chat extends Component {
         </div>
       </section>
     );
-  }  
+  }
 }
 
 export default Chat;
